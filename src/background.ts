161871f3ac9371f623d2e2d15999804c717e2337
Balu Babu <balub997@gmail.com>
2024-05-31 01:30:38 +0530

    feat: added logic to check if site has already been visited

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,42 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+// TODO: add the website to the internal history of visited sites
+// Here I need to get the URL of the current tab and add it to the history list saved in storage
+// If this is a new site, I need to create a new entry in the history list
+// If this is an existing site, I need to send a message to the content script to do the required shit
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message: string = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+const HISTORY = 'HISTORY';
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, async function (tab) {
+    if (!tab.url) return;
+
+    if (!(await isSiteVisited(tab.url))) addSiteToHistory(tab.url);
+    console.log('Site already visited');
+  });
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, change, tab) => {
+  if (tab.active && change.url) {
+    if (!tab.url) return;
+
+    if (!(await isSiteVisited(tab.url))) addSiteToHistory(tab.url);
+    console.log('Site already visited');
   }
 });
+
+const addSiteToHistory = async (currentSite: string) => {
+  const history = await getHistoryList();
+  chrome.storage.local.set({ HISTORY: [...history, currentSite] });
+};
+
+const getHistoryList = async () => {
+  const res = await chrome.storage.local.get(['HISTORY']);
+  return res.HISTORY;
+};
+
+const isSiteVisited = async (currentSite: string) => {
+  const history = await getHistoryList();
+  if (history && !history.includes(currentSite)) return false;
+  return true;
+};
